83ad99b6b9657b04d1e836fe7ad75f5352741e0a
Eunbi Kim <ekim0135@terpmail.umd.edu>
2021-10-27 00:36:45 -0400

    added simple_social_network.py

--- simple_social_network.py
+++ simple_social_network.py
@@ -9,4 +9,7 @@ class Person:
         connections (set of Person) : other people in the social network
             who know this person.
     """
-    
+    def __init__(self, name):
+        """Initialize a new Person object. """
+        self.name = name
+        self.connections = set()
